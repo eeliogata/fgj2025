f88c5d081e2046bcf23a2470874f904a3e546aed
Arpa <aarni.viljanen@gmail.com>
2025-01-24 19:27:40 +0200

    Hopefully

--- FGJ25-DD22/Assets/Scripts/MainMenu.cs
+++ FGJ25-DD22/Assets/Scripts/MainMenu.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+     //Way to load the game after pressing playbutton
+   public void PlayGame(){
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+   }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   //Way to quit the game after pressing quit
+   public void Quit(){
+    Application.Quit();
+   }
 }
